1770de5fecf87cd7cacfd6414571a6c44cfe9cd1
daylon <daylonfellipe3001@outlook.com>
2025-01-22 14:26:22 +0000

    WIP - Add Entity Task

---
+++ Source/Backend/Daylon.TaskApp.Domain/Entities/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Daylon.TaskApp.Domain.Entities
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ Source/Backend/Daylon.TaskApp.Domain/Entities/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace Daylon.TaskApp.Domain.Entities
+{
+    public class Task : EntityBase
+    {
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public bool Active { get; set; } = true;
+    }
+}
