66bc3171e02bfe17eba9d41619e5aabb8d1d4fa7
daylon <daylonfellipe3001@outlook.com>
2025-01-22 13:21:05 +0000

    WIP - Add Task Controller and Base Controller

---
+++ Source/Backend/Daylon.TaskApp.API/Controllers/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using Daylon.TaskApp.Application.UseCases.Task.Register;
+using Daylon.TaskApp.Communication.Requests;
+using Daylon.TaskApp.Communication.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Daylon.TaskApp.API.Controllers
+{
+    public class TaskController : TaskAppBaseController
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredTaskJson), StatusCodes.Status201Created)]
+        public IActionResult RegisterTask(RequestRegisterTaskJson request)
+        {
+            var useCase = new RegisterTaskUseCase();
+
+            var result = useCase.Execute(request);
+
+            return Created(string.Empty, result);
+        }
+    }
+}
